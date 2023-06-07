6fc59c6cae663df16b42c0a3e18543ccacc5d33a
Artem <artp80@gmail.com>
2023-06-07 15:08:11 +0600

    feat: add docstring for integrations

--- integrations/github.ts
+++ integrations/github.ts
@@ -1,6 +1,15 @@
 import { axiod } from 'https://deno.land/x/axiod/mod.ts';
 import { GithubRes } from './schema/githubRes.ts';
 
+/**
+Получает конфигурацию из репозитория GitHub с использованием предоставленного API-ключа, владельца репозитория, названия репозитория и пути к файлу.
+@param apiKey - API-ключ для аутентификации при доступе к репозиторию GitHub.
+@param owner - Владелец репозитория GitHub.
+@param repo - Название репозитория GitHub.
+@param filePath - Путь к файлу в репозитории GitHub.
+@returns Полученная конфигурация в формате текста.
+@throws Ошибка при возникновении проблем при получении файла из репозитория GitHub.
+*/
 export async function getGitHubConfig(
   apiKey: string,
   owner: string,
--- integrations/notion.ts
+++ integrations/notion.ts
@@ -9,15 +9,24 @@ import { jsonify } from '../helpers/stringUtils.ts';
  * @param key - Ключ аутентификации для доступа к API Notion.
  * @param blockId - Идентификатор блока Notion, содержащего конфигурацию.
  * @returns Полученная конфигурация в формате JSON.
+ * @throws Ошибка при возникновении проблем при получении файла из Notion.
  */
 export async function getNotionConfig(key: string, blockId: string) {
-  const notion = new Client({
-    auth: key,
-  });
-  const response = await notion.blocks.retrieve({
-    block_id: blockId,
-  }) as Block;
-  const strConfig = response.code.rich_text[0].plain_text;
-  const convertedText = jsonify(strConfig);
-  return convertedText;
+  try {
+    const notion = new Client({
+      auth: key,
+    });
+    const response = await notion.blocks.retrieve({
+      block_id: blockId,
+    }) as Block;
+    const strConfig = response.code.rich_text[0].plain_text;
+    const convertedText = jsonify(strConfig);
+    return convertedText;
+  } catch (error) {
+    console.error(
+      'Error occurred while fetching file from Notion:',
+      error,
+    );
+    throw error;
+  }
 }
