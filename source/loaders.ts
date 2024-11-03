39be26c9f97f5eb1c6a7dd7052da0e9120db6fcf
Artem <artp80@gmail.com>
2024-11-03 20:58:53 +0700

    fix: add absolutePathPrefix

--- source/loaders.ts
+++ source/loaders.ts
@@ -3,27 +3,21 @@
 import luminous from '@vseplet/luminous';
 import { ITunerConfig } from './type.ts';
 import { resolve } from '@std/path';
-import { findDirectoryInCWD } from './utils/pathHelper.ts';
 
 const loggerOptions = new luminous.OptionsBuilder().setName('LOADERS').build();
 const log = new luminous.Logger(loggerOptions);
 
 /**
  * Получает конфигурацию из указанного абсолютного пути.
- * @param path Путь к файлу с конфигурацией.
- * @param useAbsolutePath Использовать ли префикс file:///.
- * @returns Объект с функцией загрузки конфигурации.
  */
 const absolutePath = <T extends ITunerConfig>(
   path: string,
-  useAbsolutePath: boolean = true,
+  absolutePathPrefix?: string,
 ): { fun: () => Promise<T>; args: string; type: string } => ({
   fun: async (): Promise<T> => {
     try {
-      console.log(path)
-      const fullPath = useAbsolutePath ? `file:///${path}` : path;
-      const module = await import(fullPath);
-
+      const fullPath = absolutePathPrefix ? resolve(absolutePathPrefix, path) : path;
+      const module = await import(`file://${fullPath}`);
       return module.default as T;
     } catch (error) {
       log.err(`Error loading config from absolute path: ${path} - ${error}`);
@@ -35,22 +29,19 @@ const absolutePath = <T extends ITunerConfig>(
 });
 
 /**
- * Получает конфигурацию из файла в директории config в текущей рабочей директории.
- * @param path Относительный путь к файлу с конфигурацией.
- * @param configDirPath Путь к директории конфигурации.
- * @param useAbsolutePath Использовать ли префикс file:///.
- * @returns Объект с функцией загрузки конфигурации.
+ * Получает конфигурацию из файла в директории config.
  */
 const configDir = <T extends ITunerConfig>(
   path: string,
   configDirPath: string = './config',
-  useAbsolutePath: boolean = true,
+  absolutePathPrefix?: string,
 ): { fun: () => Promise<T>; args: string; type: string } => ({
   fun: async (): Promise<T> => {
     try {
-      const modulePath = (useAbsolutePath ? 'file:///' : '') + resolve(configDirPath, path);
-      const module = await import(modulePath);
-
+      const modulePath = absolutePathPrefix
+        ? resolve(absolutePathPrefix, configDirPath, path)
+        : resolve(configDirPath, path);
+      const module = await import(`file://${modulePath}`);
       return module.default as T;
     } catch (error) {
       log.err(`Error loading config from config directory: ${path} - ${error}`);
@@ -63,19 +54,15 @@ const configDir = <T extends ITunerConfig>(
 
 /**
  * Получает конфигурацию из файла в текущей рабочей директории.
- * @param path Относительный путь к файлу с конфигурацией в текущей рабочей директории.
- * @param useAbsolutePath Использовать ли префикс file:///.
- * @returns Объект с функцией загрузки конфигурации.
  */
 const cwd = <T extends ITunerConfig>(
   path: string,
-  useAbsolutePath: boolean = true,
+  absolutePathPrefix?: string,
 ): { fun: () => Promise<T>; args: string } => ({
   fun: async (): Promise<T> => {
     try {
-      const modulePath = (useAbsolutePath ? 'file:///' : '') + resolve('./', path);
-      const module = await import(modulePath);
-
+      const modulePath = absolutePathPrefix ? resolve(absolutePathPrefix, './', path) : resolve('./', path);
+      const module = await import(`file://${modulePath}`);
       return module.default as T;
     } catch (error) {
       log.err(`Error loading config from CWD: ${path} - ${error}`);
@@ -85,19 +72,10 @@ const cwd = <T extends ITunerConfig>(
   args: path,
 });
 
-/**
- * Загрузчик конфигурации, предоставляющий функции для получения конфигурации из различных источников.
- */
 export const Load = {
   local: {
     absolutePath,
     configDir,
     cwd,
   },
-  // remote: {
-  //   import: remoteByImport,
-  //   callbackReturnModule: remoteAsModule,
-  //   callbackReturnString: remoteAsString,
-  //   providers: {},
-  // },
 };
--- source/tuner.ts
+++ source/tuner.ts
@@ -1,3 +1,5 @@
+// source/tuner.ts
+
 import {
   CombinedConfigType,
   DeepExpand,
@@ -23,26 +25,13 @@ type ConfigList = {
  * Опции для загрузки конфигурации.
  */
 interface LoadConfigOptions {
-  /**
-   * Путь к директории конфигурации, по умолчанию './config'.
-   */
   configDirPath?: string;
-
-  /**
-   * Имя конфигурации, например, 'dev', 'prod' и т.д.
-   * Используется для указания конкретной среды.
-   */
   configName?: string;
-  useAbsolutPath?: boolean;
+  absolutePathPrefix?: string;
 }
-
 
 /**
  * Получает значение переменной окружения по указанному имени.
- * Если переменная окружения не задана, генерирует исключение MissingConfigNameEnv.
- * @param name Имя переменной окружения.
- * @returns Значение переменной окружения в виде строки.
- * @throws MissingConfigNameEnv Если переменная окружения не задана.
  */
 export function getEnv(name: string): string {
   const value = Deno.env.get(name);
@@ -50,33 +39,40 @@ export function getEnv(name: string): string {
     log.err(`Переменная окружения ${name} не задана`);
     throw new MissingConfigNameEnv(name);
   }
-  log.inf(`Переменная окружения ${name} найдена со значением ${value}`);
+
   return value;
 }
 
 /**
  * Загружает и объединяет конфигурации, учитывая наследование.
- * @param options Опции для загрузки конфигурации, включая директорию и имя конфигурации.
- * @returns Объединенная и заполненная конфигурация.
- * @throws Error при ошибке загрузки конфигурации.
  */
+
 export async function loadConfig<T>(options: LoadConfigOptions = {}): Promise<T> {
   try {
     const configDirPath = options.configDirPath || './config';
     const configName = options.configName || 'base';
-    const useAbsolutPath = options.useAbsolutPath ?? true; // по умолчанию true
     
-    const resolvedPath = resolve(configDirPath, `${configName}.tuner.ts`);
-    log.inf(`Загрузка основной конфигурации из пути: ${resolvedPath}`);
+    // Проверяем, если первый символ в absolutePathPrefix не '/', добавляем его
+    const absolutePathPrefix = options.absolutePathPrefix
+      ? options.absolutePathPrefix.startsWith('/')
+        ? options.absolutePathPrefix
+        : `/${options.absolutePathPrefix}`
+      : '';
+
+    // Используем absolutePathPrefix, если он задан, относительно текущей директории
+    const resolvedPath = absolutePathPrefix
+      ? resolve( absolutePathPrefix, configDirPath, `${configName}.tuner.ts`)
+      : resolve(configDirPath, `${configName}.tuner.ts`);
     
-    const mainConfig = await Load.local.absolutePath(resolvedPath, useAbsolutPath).fun();
-    log.inf(`Основная конфигурация загружена: ${JSON.stringify(mainConfig)}`);
-   
-    const configSequence = await inheritList(mainConfig, {}, configDirPath, useAbsolutPath);
-    log.inf(`Последовательность конфигураций: ${JSON.stringify(configSequence, null, 2)}`);
+
+    
+    const mainConfig = await Load.local.absolutePath(resolvedPath).fun();
+
+    const configSequence = await inheritList(mainConfig, {}, configDirPath, absolutePathPrefix);
+
 
     const mergedConfig = await mergeSequentialConfigs(configSequence);
-    log.inf(`Объединенная конфигурация: ${JSON.stringify(mergedConfig)}`);
+
 
     return fillEnv(mergedConfig) as T;
   } catch (error) {
@@ -87,9 +83,7 @@ export async function loadConfig<T>(options: LoadConfigOptions = {}): Promise<T>
 
 
 /**
- * Заполняет значения переменных окружения в конфигурации, если они определены как функции.
- * @param config Конфигурация с функциями в поле env.
- * @returns Конфигурация с заполненными значениями переменных окружения.
+ * Заполняет значения переменных окружения в конфигурации.
  */
 function fillEnv(config: ITunerConfig): IFilledTunerConfig {
   try {
@@ -98,7 +92,7 @@ function fillEnv(config: ITunerConfig): IFilledTunerConfig {
       const value = config.env[key];
       filledEnv[key] = typeof value === 'function' ? value(key) : value;
     }
-    log.inf(`Переменные окружения заполнены: ${JSON.stringify(filledEnv)}`);
+
     return { ...config, env: filledEnv };
   } catch (error) {
     log.err(`Ошибка заполнения переменных окружения: ${error}`);
@@ -108,9 +102,6 @@ function fillEnv(config: ITunerConfig): IFilledTunerConfig {
 
 /**
  * Рекурсивно объединяет два объекта конфигурации.
- * @param target Целевой объект, в который происходит объединение.
- * @param source Исходный объект, объединяемый с целевым.
- * @returns Объединенный объект.
  */
 function mergeRecursive(target: any, source: any): any {
   try {
@@ -130,15 +121,12 @@ function mergeRecursive(target: any, source: any): any {
 
 /**
  * Объединяет две конфигурации, включая данные и переменные окружения.
- * @param parent Родительская конфигурация.
- * @param child Дочерняя конфигурация.
- * @returns Объединенная конфигурация.
  */
 function mergeConfigs(parent: ITunerConfig, child: ITunerConfig): ITunerConfig {
   try {
     const mergedEnv = { ...parent.env, ...child.env };
     const mergedData = mergeRecursive(parent.data || {}, child.data || {});
-    log.inf(`Конфигурации объединены: ${JSON.stringify({ env: mergedEnv, data: mergedData })}`);
+
     return { env: mergedEnv, data: mergedData };
   } catch (error) {
     log.err(`Ошибка объединения конфигураций: ${error}`);
@@ -147,41 +135,35 @@ function mergeConfigs(parent: ITunerConfig, child: ITunerConfig): ITunerConfig {
 }
 
 /**
- * Получает последовательность конфигураций от дочернего к родительскому, учитывая наследование.
- * @param curConfig Текущая конфигурация.
- * @param store Массив, в который сохраняются полученные конфигурации.
- * @param configDirPath Путь к директории конфигурации.
- * @returns Массив с последовательностью конфигураций от дочернего к родительскому.
+ * Получает последовательность конфигураций от дочернего к родительскому.
  */
 async function inheritList(
   curConfig: ITunerConfig,
   store: ConfigList = {},
   configDirPath: string = './config',
-  useAbsolutPath: boolean = true,
+  absolutePathPrefix?: string,
 ): Promise<ConfigList> {
   try {
+
     store[0] = { config: curConfig, delivery: () => curConfig };
-    log.dbg(`Добавлена основная конфигурация: ${JSON.stringify(curConfig)}`);
 
     let i = 0;
     while (curConfig.child) {
       const childLoader = curConfig.child;
       let childConfig: ITunerConfig;
-      log.inf(`Загрузка дочерней конфигурации: ${JSON.stringify(childLoader)}`);
 
       if (childLoader.type === 'configDir') {
-        childConfig = await Load.local.configDir(childLoader.args, configDirPath, useAbsolutPath).fun();
+        childConfig = await Load.local.configDir(childLoader.args, configDirPath, absolutePathPrefix).fun();
       } else if (childLoader.type === 'absolutePath') {
-        childConfig = await Load.local.absolutePath(childLoader.args, useAbsolutPath).fun();
+        childConfig = await Load.local.absolutePath(childLoader.args, absolutePathPrefix).fun();
       } else {
         childConfig = await childLoader.fun();
       }
 
       store[--i] = {
         config: childConfig,
-        delivery: () => Load.local.configDir(childConfig.child?.args || '', configDirPath, useAbsolutPath).fun(),
+        delivery: () => Load.local.configDir(childConfig.child?.args || '', configDirPath, absolutePathPrefix).fun(),
       };
-      log.dbg(`Добавлена дочерняя конфигурация: ${JSON.stringify(childConfig)}`);
 
       curConfig = childConfig;
     }
@@ -191,25 +173,23 @@ async function inheritList(
     while (curConfig.parent) {
       const parentLoader = curConfig.parent;
       let parentConfig: ITunerConfig;
-      log.inf(`Загрузка родительской конфигурации: ${JSON.stringify(parentLoader)}`);
 
       if (parentLoader.type === 'configDir') {
-        parentConfig = await Load.local.configDir(parentLoader.args, configDirPath, useAbsolutPath).fun();
+        parentConfig = await Load.local.configDir(parentLoader.args, configDirPath, absolutePathPrefix).fun();
       } else if (parentLoader.type === 'absolutePath') {
-        parentConfig = await Load.local.absolutePath(parentLoader.args, useAbsolutPath).fun();
+        parentConfig = await Load.local.absolutePath(parentLoader.args, absolutePathPrefix).fun();
       } else {
         parentConfig = await parentLoader.fun();
       }
 
       store[++i] = {
         config: parentConfig,
-        delivery: () => Load.local.configDir(parentConfig.parent?.args || '', configDirPath, useAbsolutPath).fun(),
+        delivery: () => Load.local.configDir(parentConfig.parent?.args || '', configDirPath, absolutePathPrefix).fun(),
       };
-      log.dbg(`Добавлена родительская конфигурация: ${JSON.stringify(parentConfig)}`);
 
       curConfig = parentConfig;
     }
-    log.inf(`Финальная последовательность наследования конфигураций: ${JSON.stringify(store)}`);
+
     return store;
   } catch (error) {
     log.err(`Ошибка наследования конфигурации: ${error}`);
@@ -219,20 +199,18 @@ async function inheritList(
 
 /**
  * Объединяет последовательность конфигураций от дочерней к родительской.
- * @param configs Массив с последовательностью конфигураций.
- * @returns Объединенная конфигурация.
  */
 async function mergeSequentialConfigs(configs: ConfigList): Promise<ITunerConfig> {
   try {
     let mergedConfig: ITunerConfig | null = null;
     const sortedKeys = Object.keys(configs).sort((a, b) => +b - +a);
-    log.inf(`Объединение конфигураций в последовательности ключей: ${sortedKeys}`);
+
 
     for (const key of sortedKeys) {
       const currentConfig = configs[Number(key)].config;
       mergedConfig = mergedConfig === null ? currentConfig : mergeConfigs(mergedConfig, currentConfig);
     }
-    log.inf(`Конечная объединенная конфигурация: ${JSON.stringify(mergedConfig)}`);
+
     return mergedConfig as ITunerConfig;
   } catch (error) {
     log.err(`Ошибка объединения последовательных конфигураций: ${error}`);
@@ -240,12 +218,6 @@ async function mergeSequentialConfigs(configs: ConfigList): Promise<ITunerConfig
   }
 }
 
-/**
- * Возвращает объект конфигурации с расширенными типами для полей config и env.
- * @param cfg Объект конфигурации с полями config, env, child и parent.
- * @returns Объект конфигурации с расширенными типами.
- * @template T Тип объекта конфигурации.
- */
 export default function tune<T extends ITunerConfig>(cfg: T): DeepExpand<CombinedConfigType<T>> {
   return cfg as any;
 }
